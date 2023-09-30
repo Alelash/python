2cc612c571f4a6a0e8ac2383ee3ad3d6c807be07
Emil <emil@mail.ru>
2023-09-30 17:44:48 +0300

    first

---
+++ Lé filé.py
@@ -0,0 +1,15 @@
+print(69)
+print(420 * 69 + (69-69))
+print(type(5))
+print('HEEEELP11!!!HELPMEEE!!!')
+print(type("heya buddy! wanna chat!?!?!"))
+print("Hello " + "world") # конкатенация
+
+login = input("Введите логин, чтобы войти в программу!:")
+password = input("Введите Пароль, чтобы войти в программу!:")
+me = "я!!!"
+print(login, password, me)
+side = input("Введите сторону кварата:")
+side = int(side)
+s = side*side
+print("площaдь равно = " + str(s))
