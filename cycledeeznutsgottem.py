8e572aabbacd963cc102114fb4dd5f2e5d341784
цукерберг <you@example.com>
2023-10-21 17:37:52 +0300

    прошли цикл while

---
+++ cycledeeznutsgottem.py
@@ -0,0 +1,33 @@
+# counter = 1
+# while counter <= 5:
+#     print(counter)
+#     counter += 1
+
+# buter = 1
+# while buter <= 3000000000213234235436364564366354621312321123213:
+#     print(f"sdelal {buter} dollarov, policia uzhe stuchit za dveryu")
+#     buter+=231232314
+# # student = 1
+# # while student <= 6:
+# #     print (f"y Hac {student} studentov")
+# #     student+=1
+
+# import time 
+# number = 100000000000000000000000
+# while number > 0:
+#     print(number)
+#     number -= 1123123131
+# import time 
+# summa=0
+# number = 2099999
+# while number > 9:
+#     summa += number
+#     print(f"{number} vishlo {summa}")
+#     number -= 1
+rain = True
+while rain:
+    print("ya togda pyu kakao")
+    stop = input("dozhd konchilcsa (da/no)")
+    if stop == "da":
+        print ("dozda netu")
+        break
